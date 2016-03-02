5d733d5da61149a51b727b153e4faa8adaf822ae
Matt Kapp <kapp.matthew@gmail.com>
2016-03-01 18:15:41 -0700

    Initial Commit

---
+++ Savings App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Savings App
+//
+//  Created by Matt Kapp on 3/1/16.
+//  Copyright © 2016 mKappDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
